942a1aa04fdf968bbf94cf30b38b6eb7fefd343d
Akshansh <s.akshansh01@gmail.com>
2024-02-21 20:30:59 +0530

    Added models

---
+++ Backend/models/transactions.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['deposit', 'withdraw', 'transfer'],
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+    min: [1, 'Amount must be at least 1'],
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+module.exports = Transaction;
--- Backend/models/users.js
+++ Backend/models/users.js
@@ -1,28 +1,37 @@
 const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true, minlength: 3 },
     hashedPassword: { type: String, required: true },
     mobile: { type: String, required: true, unique: true, length: 10 },
-    balance: { type: Number, required: true }
+    balance: { type: Number, required: true, default:0, min: [0, 'Balance cannot be negative'] }
   });
   
 
-    async function initializePassword(password) {
-        this.hashedPassword = await bcrypt.hash(password, 10);
-    }
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('hashedPassword')) return next();
+    this.hashedPassword = await bcrypt.hash(this.hashedPassword, 10);
+    next();
+});
 
-     function deposit(amount) {
-        this.balance += amount;
-        this.transactions.push({ type: 'deposit', amount });
-    }
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.hashedPassword);
+};
+  
 
-    function withdraw(amount) {
-        if (amount > this.balance) {
-            throw new Error('Insufficient funds');
-        }
-        this.balance -= amount;
-        this.transactions.push({ type: 'withdraw', amount });
-    }
+    //  function deposit(amount) {
+    //     this.balance += amount;
+    //     this.transactions.push({ type: 'deposit', amount });
+    // }
 
-module.exports = userSchema;
+    // function withdraw(amount) {
+    //     if (amount > this.balance) {
+    //         throw new Error('Insufficient funds');
+    //     }
+    //     this.balance -= amount;
+    //     this.transactions.push({ type: 'withdraw', amount });
+    // }
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
