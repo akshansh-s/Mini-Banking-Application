4a1bb7f3839c46d0dde282f0728d2d3e49a58bfb
Akshansh <s.akshansh01@gmail.com>
2024-02-23 15:33:25 +0530

    modified code, added middleware

--- Backend/routes/authR.js
+++ Backend/routes/authR.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const authController = require('../controllers/authController');
+const authController = require('../controllers/authC.js');
 
 const router = express.Router();
 
-// Signup route
 router.post('/signup', authController.signup);
 
-// Signin route
 router.post('/signin', authController.signin);
 
 module.exports = router;
