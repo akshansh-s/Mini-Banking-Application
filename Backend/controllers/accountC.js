ed1c3bf788eeb6614024f5649c000d82172b2ed8
Akshansh <s.akshansh01@gmail.com>
2024-03-04 20:57:37 +0530

    controller added

---
+++ Backend/controllers/accountC.js
@@ -0,0 +1,40 @@
+const User = require('../models/User');
+
+exports.getBalance = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        res.json({ balance: user.balance });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
+
+exports.deposit = async (req, res) => {
+    const { amount } = req.body;
+    try {
+        let user = await User.findById(req.user.id);
+        user.balance += amount;
+        await user.save();
+        res.json({ balance: user.balance });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
+
+exports.withdraw = async (req, res) => {
+    const { amount } = req.body;
+    try {
+        let user = await User.findById(req.user.id);
+        if (user.balance < amount) {
+            return res.status(400).send('Insufficient balance');
+        }
+        user.balance -= amount;
+        await user.save();
+        res.json({ balance: user.balance });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
